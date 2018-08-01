120c1421f5ba2efb2c0fe3c881a14524b0f59547
bngoncalv <39749951+bngoncalv@users.noreply.github.com>
2018-08-01 18:45:11 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-apt-get update && apt-get install -y python-dev
-curl -O https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
-pip install awscli --upgrade
+sudo apt-get update && sudo apt-get install -y python-dev
+sudo curl -O https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo pip install awscli --upgrade
 aws --version
-aws s3 ls
+sudo aws s3 ls
