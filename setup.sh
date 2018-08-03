b8988b7ccaad819e10919c07263fce83db6d1d8b
bngoncalv <39749951+bngoncalv@users.noreply.github.com>
2018-08-03 12:02:26 -0300

    Deleting unnecessary file

--- setup.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get update && sudo apt-get install -y python-dev
-sudo curl -O https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
-sudo pip install awscli --upgrade
-aws --version
-sudo aws s3 ls
